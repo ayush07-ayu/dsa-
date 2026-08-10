f51161af414e00bae3befc8bd9c0cfa5dedbba39
Ayush sati <ayushsati019@gmail.com>
2026-08-10 23:54:38 +0530

    Time: 0 ms (100%), Space: 12.6 MB (38.71%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def isValid(self, s):
+
+        stack = []
+
+        for char in s:
+
+            if char == '(':
+                stack.append(')')
+
+            elif char == '[':
+                stack.append(']')
+
+            elif char == '{':
+                stack.append('}')
+
+            elif not stack or stack.pop() != char:
+                return False
+
+        return len(stack) == 0
