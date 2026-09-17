01fa17401703f03688c1a68cf1550c7ddaf3a672
Ayush sati <ayushsati019@gmail.com>
2026-09-17 11:41:18 +0530

    Time: 1992 ms (5.35%), Space: 0B (100%) - LeetHub

--- 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
+++ 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
@@ -1,4 +1,4 @@
-SELECT p.product_name, s.year, s.price 
-FROM Sales s
-inner join Product p
-on s.product_id= p.product_id;
+select p.product_name ,s.year  , s.price 
+from Sales s
+join Product p
+on s.product_id = p.product_id;
